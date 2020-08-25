3a1c968a6552bef9c975b83f8903cd6ae3a9ddcc
Keith <KeithD@dfo-mpo.ca>
2020-08-24 21:31:09 -0300

    Nightly push

--- Scripts/Step 6b - Model_selection_summaries.R
+++ Scripts/Step 6b - Model_selection_summaries.R
@@ -7,6 +7,7 @@ rm(list=ls())
 direct.proj <- "d:/Github"
 library(rlist)
 library(tidyverse)
+library(cowplot)
 
 # So this is the big file that has all the initial model diagnostics in it. This 
 # one wasn't included in the inital list of model diagnostics I develoepd because that is overwhelming for the dashboard
@@ -14,98 +15,175 @@ library(tidyverse)
 # load("D:/NAS/Projects/GB_time_area_closure_SPERA/Results/INLA_spatial_output.RData")
 # mod.diag.initial <- mod.diag #<- mod.diag.step.1
 # mod.diagnostics.initial <- mod.diagnostics #<- mod.diagnostics.step.1
-
 # "Only" the first 120 models count here, here's a handy function from the rlist package.
-mod.diagnostics.fe <- list.remove(mod.diagnostics.initial,121:136)
-mod.diag.fe <- do.call("rbind",mod.diagnostics.fe)
-
-
+#mod.diagnostics.fe <- list.remove(mod.diagnostics.initial,121:136)
+#mod.diag.fe <- do.call("rbind",mod.diagnostics.fe)
 #save(mod.diag.fe,file = "D:/NAS/Projects/GB_time_area_closure_SPERA/Results/FE_static_RF_model_selection.RData")
-load("D:/NAS/Projects/GB_time_area_closure_SPERA/Results/FE_static_RF_model_selection.RData")
-load("D:/NAS/Projects/GB_time_area_closure_SPERA/Results/All_model_diagnostics.RData")
+
+load("D:/Github/Paper_2_SDMs/Data/FE_static_RF_model_selection.RData")
+load("D:/Github/Paper_2_SDMs/Data/All_model_diagnostics.RData")
+
 
 # So we'll show model diagnostics for the single fixed effects and static random field as one figure with 6 panels. 3 for surveys
 # and 3 for species
 # Now we actually want to melt these so that we have the data in long form for ggplot
 mod.diag.fe <- reshape2::melt(mod.diag.fe,id.vars = c("model","species","survey","model.id"),value.name = "data", variable.name = "diag")
+mod.diag.fe$species[grepl("yt_PA",mod.diag.fe$species)] <- "Yellowtail"
+mod.diag.fe$species[grepl("cod_PA",mod.diag.fe$species)] <- "Cod"
+mod.diag.fe$model.id <- substr(mod.diag.fe$model.id,7,25) 
+
 # Ready for some loop-d-loop
 species <- unique(mod.diag.fe$species)
 num.species <- length(species)
 surveys <- unique(mod.diag.fe$survey)
 num.surveys <- length(surveys)
 
-ggplot(mod.diag.fe) + geom_point(aes(y = model.id, x = dic)) + facet_wrap(~species + survey,scales = 'free_x') 
-# So now I can make ggplots of everything, probably could pull this off in some crazy nexted object, but I don't want too..
-# I'm going to save the ggplots and plot them later
-diag.plot <- NULL
+# The lines for these figures...
+plt.min.fe.waic <- mod.diag.fe %>% filter(diag == 'waic') %>% group_by(species,survey) %>% summarise(min2= min(data)+2, min10 = min(data) + 10)
+plt.min.fe.dic <- mod.diag.fe %>% filter(diag == 'dic') %>% group_by(species,survey) %>% summarise(min2= min(data)+2, min10 = min(data) + 10)
 
-for(s in 1:num.species) 
-{
-  # Loop through each of the surveys
-  for(i in 1:num.surveys) 
-  {
-    # now only run the rest of this script if we have data
-    if((surveys[i] == 'scallop' && !grepl("PA",species[s])) || (surveys[i] != 'scallop' && grepl("PA",species[s])))
-    {
-      
-      # Now we can make the figures we need for each model, first we want to get the names of each run we did and also the number of models this includes
-      run.names <- names(mod.output)[grep(paste(species[s],surveys[i],sep=" "),names(mod.output))]
-      n.mods <- length(run.names)
-      # Now grab the model names, fortunately I made the model names with a '.' so this is pretty easy.
-      #The first ^ matches the beginning of the string, but the next one in square brackets negates, so it matches all characters EXCEPT "." - 
-      #finally the star means match that any number of times - so match everything from the start up until (but not including) the first dot. 
-      #Second argument then replaces that match with an empty string
-      mod.names <- sub("^[^.]*", "", run.names)
-      # and drop the initial period...
-      mod.names <- substr(mod.names,2,20)
-      
-      # First let's plot our model diagnostics between the models
-      # I'd like to add an h-line for the intercept model, then a line at DIC/WAIC -2 and DIC/WAIC -10
-      # to indicate which models appear to be an improvement over the intercept only model.
-      int.res <- mod.diag[mod.diag$model.id == "model.intercept",]
-      min.plus <- int.res %>%
-        group_by(diag,species,survey) %>% summarise(min2 = data - 2, min10 = data - 10) 
-      
-      # The original way I did it which looked for min model and added 2 and 10 to that model.
-      #min.plus <- mod.diag %>%
-      #  group_by(diag,species,survey) %>% summarise(min2 = min(data) + 2, min10 = min(data) + 10) 
-      
-      
-      # Don't want to do the above for the dispersion or dic's so makes those NA's
-      min.plus[min.plus$diag %in% c("dic.p.eff","waic.p.eff","Dispersion"),c("min2","min10")] <- NA
-      # Get the diagnostics data together...
-      diag.dat <- mod.diag[mod.diag$species == species[s] & mod.diag$survey == surveys[i],]
-      plt.min.plus <- min.plus[min.plus$species == species[s] & min.plus$survey == surveys[i],]
-      # Get some much more simple model names
-      diag.dat$plt.name <- substr(sub("^[^.]*", "", diag.dat$model),2,20)
-      # First we can make our diagnostic summary plot
-      pdf(file = paste0(direct.proj,"/Results/Figures/INLA/Diagnostics/Diagnositc_plots_spatial_",species[s],"_",surveys[i],".pdf"),width=14,height = 8)
-      par(mfrow = c(1,1))
-      diag.plot[[paste(species[s],surveys[i],sep="_")]] <- ggplot(diag.dat, aes(data,plt.name)) + geom_point() +  
-        geom_vline(data = plt.min.plus, aes(xintercept = min2),color="blue",linetype = "dashed",size=1) + 
-        geom_vline(data = plt.min.plus, aes(xintercept = min10),color="darkgreen",linetype = "dashed",size=1) + 
-        facet_wrap(~diag,scales = 'free_x') + xlab("") + ylab("") + ggtitle(paste("Diagnostics", species[s], surveys[i],sep=" "))+
-        theme(axis.text=element_text(size=8),axis.title=element_text(size=14,face="bold")) 
-      # Now pring the diagnostics for each survey/species combo
-      #pdf(file = paste0(direct.proj,"/Results/Figures/INLA/Diagnostics/Diagnositc_spatial_plots.pdf"),width=14,height = 8)
-      print(diag.plot[[paste(species[s],surveys[i],sep="_")]])
-      dev.off()
-      
-     
-        
-      } #end for(m in 1:n.mods)
-      dev.off()
-      
-    } # end if((surveys[i] == 'scallop' && !grepl("PA_Adu",species[s])) || (surveys[i] != 'scallop' && grepl("PA_Adu",species[s])))
-  } # end for(i in 1:num.surveys) 
-} # end for(s in 1:num.species) 
+# Now we can grab the model selection for the more complex models, first the st.10 comparisons
+mod.diag.10 <- list.remove(all.mod.diag,!grepl('st.10',names(all.mod.diag)))
+mod.diag.10 <- do.call('rbind',mod.diag.10)
+mod.diag.10 <- reshape2::melt(mod.diag.10,id.vars = c("model","species","survey","model.id"),value.name = "data", variable.name = "diag")
+mod.diag.10$species[grepl("yt_PA",mod.diag.10$species)] <- "Yellowtail"
+mod.diag.10$species[grepl("cod_PA",mod.diag.10$species)] <- "Cod"
+mod.diag.10$model.id[grepl("model.sst.sed",mod.diag.10$model.id)] <-   "SST + Sed"
+mod.diag.10$model.id[grepl("model.sst.chl",mod.diag.10$model.id)] <-   "SST + Chl"
+mod.diag.10$model.id[grepl("model.depth.chl",mod.diag.10$model.id)] <- "Dep + Chl"
+mod.diag.10$model.id[grepl("model.depth.sst",mod.diag.10$model.id)] <- "Dep + SST"
+mod.diag.10$model.id[grepl("model.depth.sed",mod.diag.10$model.id)] <- "Dep + Sed"
+mod.diag.10$model.id[grepl("model.sed.chl",mod.diag.10$model.id)] <-   "Chl + Sed"
+mod.diag.10$model.id[grepl("model.sst",mod.diag.10$model.id)] <- "SST"
+mod.diag.10$model.id[grepl("model.depth",mod.diag.10$model.id)] <- "Dep"
+mod.diag.10$model.id[grepl("model.chl",mod.diag.10$model.id)] <- "Chl"
+mod.diag.10$model.id[grepl("model.sed",mod.diag.10$model.id)] <- "Sed"
+mod.diag.10$model.id[grepl("model.int",mod.diag.10$model.id)] <- "Intercept"
+mod.diag.10$era <- 10
+
+plt.min.10.waic <- mod.diag.10 %>% filter(diag == 'waic') %>% group_by(species,survey) %>% summarise(min2= min(data)+2, min10 = min(data) + 10)
+plt.min.10.dic <- mod.diag.10 %>% filter(diag == 'dic') %>% group_by(species,survey) %>% summarise(min2= min(data)+2, min10 = min(data) + 10)
+
+mod.diag.5 <- list.remove(all.mod.diag,!grepl('st.5',names(all.mod.diag)))
+mod.diag.5 <- do.call('rbind',mod.diag.5)
+mod.diag.5 <- reshape2::melt(mod.diag.5,id.vars = c("model","species","survey","model.id"),value.name = "data", variable.name = "diag")
+mod.diag.5$species[grepl("yt_PA",mod.diag.5$species)] <- "Yellowtail"
+mod.diag.5$species[grepl("cod_PA",mod.diag.5$species)] <- "Cod"
+mod.diag.5$model.id[grepl("model.depth.sst.chl",mod.diag.5$model.id)] <- "Dep + SST + Chl"
+mod.diag.5$model.id[grepl("model.depth.sed.sst",mod.diag.5$model.id)] <- "Dep + SST + Sed"
+mod.diag.5$model.id[grepl("model.depth.sst",mod.diag.5$model.id)] <- "Dep + SST"
+mod.diag.5$model.id[grepl("model.depth.sed",mod.diag.5$model.id)] <- "Dep + Sed"
+mod.diag.5$model.id[grepl("model.int",mod.diag.5$model.id)] <- "Intercept"
+mod.diag.5$era <- 5
+
+plt.min.5.waic <- mod.diag.5 %>% filter(diag == 'waic') %>% group_by(species,survey) %>% summarise(min2= min(data)+2, min10 = min(data) + 10)
+plt.min.5.dic <- mod.diag.5 %>% filter(diag == 'dic') %>% group_by(species,survey) %>% summarise(min2= min(data)+2, min10 = min(data) + 10)
+
+# These model 3's aren't necessary really as they are only comparing the covariate model to an intercept only model.
+# But the intercept model is useful for us to compare the random fields later...
+mod.diag.3 <- list.remove(all.mod.diag,!grepl('st.3',names(all.mod.diag)))
+mod.diag.3 <- do.call('rbind',mod.diag.3)
+mod.diag.3 <- reshape2::melt(mod.diag.3,id.vars = c("model","species","survey","model.id"),value.name = "data", variable.name = "diag")
+mod.diag.3$species[grepl("yt_PA",mod.diag.3$species)] <- "Yellowtail"
+mod.diag.3$species[grepl("cod_PA",mod.diag.3$species)] <- "Cod"
+mod.diag.3$model.id[grepl("model.int",mod.diag.3$model.id)] <- "Intercept"
+mod.diag.3$model.id[grepl("model.depth.sst",mod.diag.3$model.id)] <- "Dep + SST"
+mod.diag.3$model.id[grepl("model.depth.sed.sst",mod.diag.3$model.id)] <- "Dep + SST + Sed"
+mod.diag.3$era <- 3
 
 
+plt.min.3.waic <- mod.diag.3 %>% filter(diag == 'waic') %>% group_by(species,survey) %>% summarise(min2= min(data)+2, min10 = min(data) + 10)
+plt.min.3.dic <- mod.diag.3 %>% filter(diag == 'dic') %>% group_by(species,survey) %>% summarise(min2= min(data)+2, min10 = min(data) + 10)
+
+# Finally we compare the random fields in one object, just comparing the intercept models for simplicity.
+mod.diag.rf <- bind_rows(mod.diag.10 %>% filter(model.id == "Dep + SST"),
+                         mod.diag.5 %>% filter(model.id == "Dep + SST"),
+                         mod.diag.5 %>% filter(model.id == "Dep + SST + Sed"),
+                         mod.diag.3 %>% filter(model.id == "Dep + SST"),
+                         mod.diag.3 %>% filter(model.id == "Dep + SST + Sed"),)
+mod.diag.rf$era <- as.factor(mod.diag.rf$era)
+plt.min.rf.cod.waic <- mod.diag.rf %>% filter(diag == 'waic' & species== 'Cod') %>% group_by(survey) %>% summarise(min2= min(data)+2, min10 = min(data) + 10)
+plt.min.rf.cod.dic <- mod.diag.rf %>% filter(diag == 'dic'& species == 'Cod') %>% group_by(survey) %>% summarise(min2= min(data)+2, min10 = min(data) + 10)
+
+plt.min.rf.yt.3.5.waic <- mod.diag.rf %>% filter(diag == 'waic' & model.id == "Dep + SST + Sed") %>% group_by(survey) %>% summarise(min2= min(data)+2, min10 = min(data) + 10)
+plt.min.rf.yt.3.5.dic <- mod.diag.rf %>% filter(diag == 'dic'& model.id == "Dep + SST + Sed") %>% group_by(survey) %>% summarise(min2= min(data)+2, min10 = min(data) + 10)
+# and yellowtail 5 and 10 year comparison
+plt.min.rf.yt.5.10.waic <- mod.diag.rf %>% filter(diag == 'waic' & model.id == "Dep + SST" & species == "Yellowtail") %>% group_by(survey) %>% summarise(min2= min(data)+2, min10 = min(data) + 10)
+plt.min.rf.yt.5.10.dic <- mod.diag.rf %>% filter(diag == 'dic'& model.id == "Dep + SST" & species == "Yellowtail") %>% group_by(survey) %>% summarise(min2= min(data)+2, min10 = min(data) + 10)
 
 
+# The base models using a fixed random field and single covariates, WAIC then DIC
+plt.waic.fe <- ggplot(mod.diag.fe %>% filter(diag == 'waic')) + geom_point(aes(y = model.id, x = data)) + 
+  facet_wrap(~species + survey,scales = 'free_x') + 
+  geom_vline(data = plt.min.fe.waic, aes(xintercept = min10),color="darkgreen",linetype = "dashed",size=1) + 
+  geom_vline(data = plt.min.fe.waic, aes(xintercept = min2),color="blue",linetype = "dashed",size=1)  
+
+plt.dic.fe <- ggplot(mod.diag.fe %>% filter(diag == 'dic')) + geom_point(aes(y = model.id, x = data)) + 
+  facet_wrap(~species + survey,scales = 'free_x') + 
+  geom_vline(data = plt.min.fe.dic, aes(xintercept = min10),color="darkgreen",linetype = "dashed",size=1) + 
+  geom_vline(data = plt.min.fe.dic, aes(xintercept = min2),color="blue",linetype = "dashed",size=1)  
+
+# More complex models using the 10 year random field
+plt.waic.10 <- ggplot(mod.diag.10 %>% filter(diag == 'waic')) + geom_point(aes(y = model.id, x = data)) + 
+  facet_wrap(~species + survey,scales = 'free_x') + xlab("WAIC") + ylab("") +
+  geom_vline(data = plt.min.10.waic, aes(xintercept = min10),color="darkgreen",linetype = "dashed",size=1) + 
+  geom_vline(data = plt.min.10.waic, aes(xintercept = min2),color="blue",linetype = "dashed",size=1)  
+
+plt.dic.10 <- ggplot(mod.diag.10 %>% filter(diag == 'dic')) + geom_point(aes(y = model.id, x = data)) + 
+  facet_wrap(~species + survey,scales = 'free_x') + xlab("DIC") + ylab("")+
+  geom_vline(data = plt.min.10.dic, aes(xintercept = min10),color="darkgreen",linetype = "dashed",size=1) + 
+  geom_vline(data = plt.min.10.dic, aes(xintercept = min2),color="blue",linetype = "dashed",size=1)  
+
+# More complex models using the 5 year random field
+plt.waic.5 <- ggplot(mod.diag.5 %>% filter(diag == 'waic')) + geom_point(aes(y = model.id, x = data)) + 
+  facet_wrap(~species + survey,scales = 'free') + 
+  geom_vline(data = plt.min.5.waic, aes(xintercept = min10),color="darkgreen",linetype = "dashed",size=1) + 
+  geom_vline(data = plt.min.5.waic, aes(xintercept = min2),color="blue",linetype = "dashed",size=1)  
+
+plt.dic.5 <- ggplot(mod.diag.5 %>% filter(diag == 'dic')) + geom_point(aes(y = model.id, x = data)) + 
+  facet_wrap(~species + survey,scales = 'free') + 
+  geom_vline(data = plt.min.5.dic, aes(xintercept = min10),color="darkgreen",linetype = "dashed",size=1) + 
+  geom_vline(data = plt.min.5.dic, aes(xintercept = min2),color="blue",linetype = "dashed",size=1)  
 
 
+# Comparing the random field intercept only models.
+plt.cod.waic.rf <- ggplot(mod.diag.rf %>% filter(diag == 'waic' & species == 'Cod')) + geom_point(aes(y = era, x = data)) + 
+  facet_wrap(~survey ,scales = 'free') +  xlab("WAIC")+
+  geom_vline(data = plt.min.rf.cod.waic, aes(xintercept = min10),color="darkgreen",linetype = "dashed",size=1) + 
+  geom_vline(data = plt.min.rf.cod.waic, aes(xintercept = min2),color="blue",linetype = "dashed",size=1)  
 
+plt.cod.dic.rf <- ggplot(mod.diag.rf %>% filter(diag == 'dic' & species == 'Cod')) + geom_point(aes(y = era, x = data)) + 
+  facet_wrap(~ survey,scales = 'free') +  xlab("DIC")+
+  geom_vline(data = plt.min.rf.cod.dic, aes(xintercept = min10),color="darkgreen",linetype = "dashed",size=1) + 
+  geom_vline(data = plt.min.rf.cod.dic, aes(xintercept = min2),color="blue",linetype = "dashed",size=1)  
+
+# Similar for yt but needs to be done in two pieces, compare 10 and 5 year models then 5 and 3 year models.
+plt.yt.5.10.waic.rf <- ggplot(mod.diag.rf %>% filter(diag == 'waic' & species == 'Yellowtail' & model.id == "Dep + SST")) + geom_point(aes(y = era, x = data)) + 
+  facet_wrap(~survey ,scales = 'free') + xlab("")+
+  geom_vline(data = plt.min.rf.yt.5.10.waic, aes(xintercept = min10),color="darkgreen",linetype = "dashed",size=1) + 
+  geom_vline(data = plt.min.rf.yt.5.10.waic, aes(xintercept = min2),color="blue",linetype = "dashed",size=1)  
+
+plt.yt.5.10.dic.rf <- ggplot(mod.diag.rf %>% filter(diag == 'dic'& species == 'Yellowtail' & model.id == "Dep + SST")) + geom_point(aes(y = era, x = data)) + 
+  facet_wrap(~ survey,scales = 'free') + xlab("")+
+  geom_vline(data = plt.min.rf.yt.5.10.dic, aes(xintercept = min10),color="darkgreen",linetype = "dashed",size=1) + 
+  geom_vline(data = plt.min.rf.yt.5.10.dic, aes(xintercept = min2),color="blue",linetype = "dashed",size=1)  
+
+
+plt.yt.3.5.waic.rf <- ggplot(mod.diag.rf %>% filter(diag == 'waic' & species == 'Yellowtail' & model.id == "Dep + SST + Sed")) + geom_point(aes(y = era, x = data)) + 
+  facet_wrap(~survey ,scales = 'free') + xlab("WAIC")+
+  geom_vline(data = plt.min.rf.yt.3.5.waic, aes(xintercept = min10),color="darkgreen",linetype = "dashed",size=1) + 
+  geom_vline(data = plt.min.rf.yt.3.5.waic, aes(xintercept = min2),color="blue",linetype = "dashed",size=1)  
+
+plt.yt.3.5.dic.rf <- ggplot(mod.diag.rf %>% filter(diag == 'dic'& species == 'Yellowtail' & model.id == "Dep + SST + Sed")) + geom_point(aes(y = era, x = data)) + 
+  facet_wrap(~ survey,scales = 'free') + xlab("DIC")+
+  geom_vline(data = plt.min.rf.yt.3.5.dic, aes(xintercept = min10),color="darkgreen",linetype = "dashed",size=1) + 
+  geom_vline(data = plt.min.rf.yt.3.5.dic, aes(xintercept = min2),color="blue",linetype = "dashed",size=1)  
+
+plt.yt.rf.waic <- plot_grid(plt.yt.5.10.waic.rf,plt.yt.3.5.waic.rf,nrow=2)
+plt.yt.rf.dic <- plot_grid(plt.yt.5.10.dic.rf,plt.yt.3.5.dic.rf,nrow=2)
+
+#save.image("D:/Github/Paper_2_SDMs/Data/model_diagnostics_for_papers.RData")
+load("D:/Github/Paper_2_SDMs/data/model_diagnostics_for_papers.RData")
 
 
 
